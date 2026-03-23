23e3793149051bbb2a516b3c943f4c7d9664d333
CEE <archmagece@users.noreply.github.com>
2026-03-23 16:01:16 +0900

    chor: verup

--- internal/config/version.go
+++ internal/config/version.go
@@ -2,7 +2,7 @@ package config
 
 var (
 	// Version is the current DVA version.
-	Version = "0.1.11"
+	Version = "0.1.12"
 	// Commit is the git commit hash at build time.
 	Commit = "none"
 	// BuildDate is the date and time of the build.
