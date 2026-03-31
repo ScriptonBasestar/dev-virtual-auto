3c5b3c7a2cd225763cb41462e839e72e8e9aa274
CEE <archmagece@users.noreply.github.com>
2026-03-31 22:27:38 +0900

    chore: bump version to 0.1.45

--- internal/config/version.go
+++ internal/config/version.go
@@ -2,7 +2,7 @@ package config
 
 var (
 	// Version is the current DVA version.
-	Version = "0.1.44"
+	Version = "0.1.45"
 	// Commit is the git commit hash at build time.
 	Commit = "none"
 	// BuildDate is the date and time of the build.
